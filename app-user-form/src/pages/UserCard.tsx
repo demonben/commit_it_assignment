61e771cfa0b31c3156ec8450ffdfe59551ab62ea
Dima <benensondimitrii@gmail.com>
2022-06-16 14:46:16 +0300

    updated UserCard page added user card

--- app-user-form/src/pages/UserCard.tsx
+++ app-user-form/src/pages/UserCard.tsx
@@ -1,6 +1,24 @@
 import React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
 
 const UserCard = () => {
-  return <div>UserCard</div>;
+  return (
+    <div>
+      <Card>
+        <CardContent>
+          <div className="user-card">
+            <Typography>User name:</Typography>
+            <Typography> Dima </Typography>
+          </div>
+          <div className="user-card">
+            <Typography>Phone Number:</Typography>
+            <Typography> 1234567890</Typography>
+          </div>
+        </CardContent>
+      </Card>
+    </div>
+  );
 };
 export default UserCard;
