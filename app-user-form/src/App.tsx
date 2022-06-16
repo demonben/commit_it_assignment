da96aec0a6c172766bf102689e4fac96e774dff1
Dima <benensondimitrii@gmail.com>
2022-06-16 14:22:13 +0300

    created tabs panel

--- app-user-form/src/App.tsx
+++ app-user-form/src/App.tsx
@@ -1,8 +1,43 @@
 import React from "react";
 import "./App.css";
+import TabPanel from "./components/TabPanel";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Box from "@mui/material/Box";
 
-function App() {
-  return <div className="App"></div>;
+
+function a11yProps(index: any) {
+  return {
+    id: `simple-tab-${index}`,
+    "aria-controls": `simple-tabpanel-${index}`,
+  };
 }
 
+function App() {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+  return (
+    <div className="App">
+      <Box sx={{ width: "100%" }}>
+        <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+          <Tabs
+            value={value}
+            onChange={handleChange}
+            aria-label="basic tabs example"
+          >
+            <Tab label="FORM" {...a11yProps(0)} />
+            <Tab label="USER" {...a11yProps(1)} />
+          </Tabs>
+        </Box>
+        <TabPanel value={value} index={0}>
+        </TabPanel>
+        <TabPanel value={value} index={1}>
+        </TabPanel>
+      </Box>
+    </div>
+  );
+}
 export default App;
